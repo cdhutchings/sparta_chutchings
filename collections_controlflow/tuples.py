ab26f352cd1ef2187d8e8681eec95386f73dc062
Charles Hutchings <CHutchings@spartaglobal.com>
2019-05-30 11:27:40 +0100

    added tuples exercise

---
+++ collections_controlflow/tuples.py
@@ -0,0 +1,11 @@
+# Tuples
+# Immutable lists
+# Accessed using index
+# Defined with ordinary brackets ()
+
+mortal_enemies = ('HELLO KITTY', 'SAILOR MOON', 'CAPTAIN AMERICA', 'JERRY')
+
+print(mortal_enemies)
+print(type(mortal_enemies))
+
+#mortal_enemies[1] = 'GOKU' # Not allowed, since tuples are immutable
